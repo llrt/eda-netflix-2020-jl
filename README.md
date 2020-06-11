527722c564e8a65e004885ba42a3da947a64eab2
llrt <leandroloriato@gmail.com>
2020-06-11 15:34:10 -0300

    Adding some univariate exploration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # eda-netflix-2020-jl
 
-This is a loose port to Julia of [excellent EDA of Netflix 2020 Dataset in R by ygterl](https://github.com/ygterl/EDA-Netflix-2020-in-R) . His original Medium post is avalaible [here](https://medium.com/deep-learning-turkiye/exploration-of-netflix-2020-dataset-in-r-markdown-eda-b202bbaec4a)
+This is a loosely inspired port of [great EDA of Netflix 2020 Dataset in R by ygterl](https://github.com/ygterl/EDA-Netflix-2020-in-R)  to Julia. His original Medium post is avalaible [here](https://medium.com/deep-learning-turkiye/exploration-of-netflix-2020-dataset-in-r-markdown-eda-b202bbaec4a)
+
+The main goal of this port is to explore Julia's data manipulation/plotting libraries.
--- eda.jl
+++ eda.jl
@@ -5,10 +5,11 @@ using Queryverse # metapackage for DS
 using DataFrames, CategoricalArrays # packages for data frame manipulation
 using CSV # package for CSV loading
 using StatsKit # metapackage for statistics
+using Gadfly # package for plotting
 
 
 # reading data file
-netflix_df = CSV.read("./netflix_titles.csv", delim=',', header=1) |> DataFrame
+netflix_df = CSV.read("./netflix_titles.csv", delim=',', header=1, dateformat="U d, Y") |> DataFrame
 
 # next, it's useful to take a good look at the dataset
 println("Number of obs: ", nrow(netflix_df)) # number of observations
@@ -22,19 +23,9 @@ last(netflix_df, 5) # listing last 5 obs
 # discard show id, it's not really relevant
 netflix_df = netflix_df |> @select(-:show_id) |> DataFrame
 
-# change columns to categorical variables (factors)
-# categorical!(netflix_df, :type, compress=true) # movie's type
-# categorical!(netflix_df, :country, compress=true) # movie's country
-# categorical!(netflix_df, :rating, compress=true) # movie's mature rating
-# categorical!(netflix_df, :release_year, compress=true) # movie's release year
-# categorical!(netflix_df, :listed_in, compress=true) # movie's genre
-# categorical!(netflix_df, :director, compress=true) # movie's director
-
-#println("Cols' types: ", [eltype(col) for col = eachcol(netflix_df)]) # list variables types again, after changing to categoricals
-
 # investigate number of missings in each column
 for col in names(netflix_df)
-    println("number of missings in $col column: ", count(ismissing, netflix_df[:, col]))
+    println("number of missings in $col col: ", count(ismissing, netflix_df[:, col]))
 end
 
 # investigate number of complete cases (with no missing values in any column)
@@ -44,5 +35,51 @@ dropmissing!(netflix_df) # retaining only complete cases
 
 # investigate most common values in each columns
 for col in names(netflix_df)
-    println("most common value in $col column: ", modes(netflix_df[:, col]))
+    println("most common value in $col col: ", modes(netflix_df[:, col]))
 end
+
+# change columns to categorical variables (factors)
+categorical!(netflix_df, :type, compress=true) # title's type
+categorical!(netflix_df, :country, compress=true) # title's country
+categorical!(netflix_df, :rating, compress=true) # movie's mature rating
+categorical!(netflix_df, :release_year, compress=true) # movie's release year
+categorical!(netflix_df, :listed_in, compress=true) # movie's genre
+categorical!(netflix_df, :director, compress=true) # movie's director
+
+
+# explore individual feature (univariate) distribution
+set_default_plot_size(14cm, 8cm)
+
+# 1: movie's type
+type_df = netflix_df |> 
+    @groupby(_.type) |>
+    @map({type=key(_), count=length(_)}) |> DataFrame
+
+print(type_df)
+    
+plot(type_df, y=:type, x=:count, 
+     Geom.bar(orientation=:horizontal),
+     Theme(bar_spacing=0.5cm),
+     Guide.title("Titles by types"))
+
+# 2: movie's country
+# unfornately, country is a comma separated column - we'll have to split it
+
+countries_arr = map(x -> split(x, ", "), netflix_df.country)
+countries = unique(reduce(append!, countries_arr, init=[]))
+
+countries_dict = Dict()
+for c in countries
+    countries_dict[c] = length(filter(l -> c in l, countries_arr))
+end
+
+countries_df = 
+        DataFrame(
+            country=collect(String, keys(countries_dict)), 
+            count=collect(Int, values(countries_dict))) |>
+        @orderby_descending(_.count) |> DataFrame
+
+plot(first(countries_df, 10), y=:country, x=:count, 
+     Geom.bar(orientation=:horizontal),
+     Theme(bar_spacing=0.5cm),
+     Guide.title("Top 10 countries"))
